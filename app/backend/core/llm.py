9251ecc17ef135ef7928c2cdedf8496379e750fa
Sithi <sithivignesh11@gmail.com>
2026-06-22 10:58:26 +0530

    Remove test invoke line from llm.py

--- app/backend/core/llm.py
+++ app/backend/core/llm.py
@@ -2,5 +2,3 @@ from langchain_groq import ChatGroq
 from app.backend.config.settings import GROQ_API_KEY
 
 llm = ChatGroq(api_key=GROQ_API_KEY, model_name="llama-3.3-70b-versatile")
-
-print(llm.invoke("say hello"))
