f687fd4ec54cdabe930581a8ecde132902410cc7
Sithi <sithivignesh11@gmail.com>
2026-06-23 13:22:07 +0530

    added core_memory >> - added my personal info as a statuc JSON format >> - this Always injected into context, never retrieved via RAG

---
+++ app/backend/memory/core_memory.py
@@ -0,0 +1,13 @@
+CORE_MEMORY = {
+    "name": "Thunder",
+    "real_name": "Sithi Vignesh",
+    "college": "VIT Vellore",
+    "college_year": "3rd year",
+    "branch": "CSE AI/ML",
+    "goals": ["placement", "build Cyclone", "get certifications", "get a girlfriend", "watch a lot of movies, series and anime", "go on a journey with friends"],
+    "personality": ["overthinker", "daydreamer", "funny", "hopeless romantic"],
+    "about":"""<you will know it soon>"""
+}
+
+def load_core_memory():
+    return CORE_MEMORY
