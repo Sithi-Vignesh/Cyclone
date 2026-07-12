411a3d0e72488a8487d0b17684bb3204ca533672
Sithi <sithivignesh11@gmail.com>
2026-07-12 12:38:21 +0530

    chore(config): add GROQ_API_KEY5/6 and OPENROUTER_API_KEY4/5/6

--- app/backend/config/settings.py
+++ app/backend/config/settings.py
@@ -16,4 +16,7 @@ OPENROUTER_API_KEYS = [
     os.getenv("OPENROUTER_API_KEY1"),
     os.getenv("OPENROUTER_API_KEY2"),
     os.getenv("OPENROUTER_API_KEY3"),
+    os.getenv("OPENROUTER_API_KEY4"),
+    os.getenv("OPENROUTER_API_KEY5"),
+    os.getenv("OPENROUTER_API_KEY6"),
 ]
